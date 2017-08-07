22d29ba240d774df46b475d1b89cf2f17da69e75
John Demmitt <john_demmitt@outlook.com>
2017-08-07 14:31:45 -0700

    more demos

--- car-anim-partials/esv-mbz-reverse-ani/js/rev-script.js
+++ car-anim-partials/esv-mbz-reverse-ani/js/rev-script.js
@@ -14,6 +14,8 @@ jQuery(document).ready(function($) {
     var mbzMove = TweenLite.fromTo($("#mbz"), 5, { x: "-400vw" }, { x: "400vw" });
     var wheelRot = TweenLite.to([fWheel, rWheel], 5, { rotation: -4800, transformOrigin: "50% 50%" });
     var esvMove = TweenLite.fromTo($("#esv"), 5, { x: "400vw" }, { x: "-400vw" });*/
+
+    
     var rev = new TimelineMax();
     rev.to(esv, 0.1, { rotationY: 180 }, "prep").to(esv, 0.1, { y: "-=160" }, "prep").to(mbz, 0.1, { rotationY: 180 }, "prep").to(mbz, 0.1, { y: "+=180" }, "prep").fromTo(mbz, 4, { x: width }, { x: -width }, "reverse")
         .to([fRim, rRim], 4, { rotation: -4000, transformOrigin: "50% 50%" }, "reverse")
